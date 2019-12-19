54f37bf61fb0443b372883d968310ade57fcd36e
victor <victorokr@hotmail.com>
2019-12-19 15:48:21 -0500

    menu terminado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,10 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" ></script>
+
+    <!--iconos desde internet font Awesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -26,10 +29,17 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color: #fff;">
             <div class="container">
+
                 <a class="navbar-brand" href="{{ url('home') }}">
-                   
                     <img class="navbar-light" src="/images/logoCol.png">
                 </a>
+
+                @if (auth()->check())
+                <a class="navbar-brand">
+                    <button type="button" title="Menu" class="openbtn btn btn-light" onclick="openNav()"><i class="fas fa-align-justify"></i></button>
+                </a>
+                @endif
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -49,16 +59,16 @@
                                 
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('home') }}">{{ __('Home') }}</a>
+                                <a class="nav-link" href="{{ route('home') }}"><i class="fas fa-home"></i> {{ __('Home') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Docentes') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-user-friends"></i> {{ __('Docentes') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('mensajes.create') }}">{{ __('Contacto') }}</a>
+                                <a class="nav-link" href="{{ route('mensajes.create') }}"><i class="fas fa-envelope-open-text"></i> {{ __('Contacto') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ url('acudientes/login') }}">{{ __('Acceso Acudientes') }}</a>
+                                <a class="nav-link" href="{{ url('acudientes/login') }}"><i class="fas fa-sign-in-alt"></i> {{ __('Acceso Acudientes') }}</a>
                             </li>
                             
                             
@@ -72,7 +82,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Cerrar Sesion') }}
+                                       <i class="fas fa-sign-out-alt"></i> {{ __('Cerrar Sesion') }} 
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -87,8 +97,141 @@
         </nav>
 
         <main class="py-4">
+        {{-- menu con jquery --}} 
+        @if (auth()->check()) 
+        <div class="contenedor-menu">
+          <div class="row">
+            <div class="col-lg-12">
+              <div id="mySidebar" class="sidebar">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+               
+               <a href="#" class="btn-menu">Menu<i class="icono fas fa-bars"></i></a> <!--enlace para dispositivos moviles, con icono fas fa-bars-->
+               <ul class="menu">
+                    
+                    <li><a href="#"><i class="icono izquierda fas fa-chalkboard-teacher"></i> Docentes <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Crear Docente</a></li>
+                            <li><a href="#">Lista Docentes</a></li>    
+                        </ul>
+                    </li>
+
+                    <li><a href="#"><i class="icono izquierda fas fa-book-open"></i> Asignaturas <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Crear Asignatura</a>
+                            <li><a href="#">Lista de Asignaturas</a>
+                        </ul>
+                    </li>
+                    
+
+
+                    
+                    <li><a href="#"><i class="icono izquierda fas fa-bookmark"></i> Logros <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Crear Logro</a>
+                            <li><a href="#">Lista de Logros</a>
+                        </ul>
+                    </li>
+                    <li><a href="#"><i class="icono izquierda fas fa-pencil-alt"></i> Calificaciones <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            {{-- <li><a href="#">Agregar Calificaciones</a></li> --}}
+                            <li><a href="#">Lista de Calificaciones</a></li> 
+                        </ul>
+                    </li>
+
+                    <li><a href="#"><i class="icono izquierda fas fa-building"></i> Cursos <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Agregar Curso</a>
+                            <li><a href="#">Lista de Cursos</a>
+                        </ul>
+                    </li>
+
+                    <li><a href="#"><i class="icono izquierda far fa-credit-card"></i> Matricula <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Agregar Matricula</a>
+                            <li><a href="#">Lista de Matriculas</a>
+                        </ul>
+                    </li>
+
+                    <li><a href="#"><i class="icono izquierda fas fa-users"></i> Alumnos <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Agregar Alumno</a>
+                            <li><a href="#">Lista de Alumnos</a>
+                        </ul>
+                    </li>
+
+                    <li><a href="#"><i class="icono izquierda fas fa-restroom"></i> Acudientes <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Agregar Acudiente</a>
+                            <li><a href="#">Lista de Acudientes</a>
+                        </ul>
+                    </li>
+
+
+                    <li><a href="#"><i class="icono izquierda fas fa-cloud-upload-alt"></i> Cargar Calificaciones <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Cargar Calificaciones</a>
+                            
+                        </ul> 
+                    </li>
+                   
+
+
+                    
+                    <li><a href="#"><i class="icono izquierda fas fa-book-reader"></i> Item <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Item 1</a>
+                            
+                        </ul>
+                    </li>
+                    <li><a href="#"><i class="icono izquierda fas fa-book"></i> Item<i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Item 1</a>
+                            
+                        </ul>
+                    </li>
+                    
+
+                    
+
+                    
+                    <li><a href="#"><i class="icono izquierda fas fa-box-open"></i>Item <i class="icono derecha fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="#">Item 1 </a>
+                                
+                            {{-- <li><a href="#">Registrar Entradas</a>
+                            <li><a href="#">Registrar Traslados</a> --}}    
+                        </ul>
+
+                    </li> 
+                    
+                </ul>   
+            
+              </div>
+            </div>
+          </div>
+        </div>
+        @endif
             @yield('content')
         </main>
     </div>
+
+<script>
+    /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "220px";
+  document.getElementById("app").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("app").style.marginLeft = "0";
+}
+</script>
+
+
+
+<script src="{{ asset('js/jquery-3.4.1.min.js') }}" ></script>
+<script src="{{ asset('js/main.js') }}" defer></script>
 </body>
 </html>
