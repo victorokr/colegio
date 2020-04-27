2b62a56ec3a1ab6e4dc768a1ad885bcb55b54a25
Diego Portela <www.diego_portela@hotmail.com>
2020-04-26 22:57:40 -0500

    primer cambio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,7 @@
    <div class="card-group ">
 	  <div class="card card border-light ">
 	    <div class="card-body">
-	      <h5 class="card-title">ABOUT ME</h5>
+	      <h5 class="card-title">SOFTCODE</h5>
 	      <p class="card-text ">Hello, I’m a UI/UX Designer & Front End Developer from Victoria, Australia. I hold a master degree of Web Design from the World University.And scrambled it to make a type specimen book. It has survived not only five centuries
 	      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, adipisci voluptatum repudiandae, natus impedit repellat aut officia illum at assumenda iusto reiciendis placeat. Temporibus, vero..</p>
 	      
